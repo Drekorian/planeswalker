e89df2b074c4693e51e4ab1fbb720009017f70d6
Marek Osvald <marek.osvald@greencode.cz>
2024-11-12 07:51:39 +0100

    Migrate Koin DSL API to viewModelOf.

--- app/app-vanilla/src/main/kotlin/cz/drekorian/android/planeswalker/di/module/AppVanillaModule.kt
+++ app/app-vanilla/src/main/kotlin/cz/drekorian/android/planeswalker/di/module/AppVanillaModule.kt
@@ -2,49 +2,28 @@ package cz.drekorian.android.planeswalker.di.module
 
 import coil.Coil
 import cz.drekorian.android.planeswalker.card.CardViewModel
-import cz.drekorian.android.planeswalker.lifecounter.LifeCounterActivity
 import cz.drekorian.android.planeswalker.lifecounter.LifeCounterViewModel
 import cz.drekorian.android.planeswalker.main.MainViewModel
 import cz.drekorian.android.planeswalker.set.SetViewModel
 import cz.drekorian.android.planeswalker.set.list.SetListViewModel
 import cz.drekorian.android.planeswalker.settings.SettingsViewModel
 import cz.drekorian.android.planeswalker.settings.life.SettingsLifeViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.qualifier.TypeQualifier
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 /**
- * This module provides Coil-related dependencies.
+ * This module provides vanilla flavor dependencies.
  */
 val appVanillaModule = module {
 
     single { Coil }
 
-    viewModel {
-        CardViewModel(get())
-    }
-
-    viewModel {
-        LifeCounterViewModel(get())
-    }
-
-    viewModel {
-        MainViewModel(get())
-    }
-
-    viewModel {
-        SettingsLifeViewModel(get())
-    }
-
-    viewModel {
-        SettingsViewModel(get(), get())
-    }
-
-    viewModel {
-        SetListViewModel(get())
-    }
-
-    viewModel {
-        SetViewModel(get())
-    }
+    viewModelOf(::CardViewModel)
+    viewModelOf(::LifeCounterViewModel)
+    viewModelOf(::MainViewModel)
+    viewModelOf(::SettingsLifeViewModel)
+    viewModelOf(::SettingsViewModel)
+    viewModelOf(::SettingsViewModel)
+    viewModelOf(::SetListViewModel)
+    viewModelOf(::SetViewModel)
 }
